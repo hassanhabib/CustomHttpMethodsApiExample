b11f15860ed60dd69cbbc7890a7a6e0a55d34dd1
Hassan Habib <hassanhabib@live.com>
2024-05-19 18:33:49 -0700

    Add project files.

---
+++ REST3DemoAPi/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+using RESTFulSense.Models.Attributes;
+
+namespace REST3DemoAPi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpCustom("CUSTOM")]
+        public ActionResult<Student> Get() => Ok(new Student
+        {
+            Id = 1
+        });
+    }
+
+    public class Student
+    {
+        public int Id { get; set; }
+    }
+}
